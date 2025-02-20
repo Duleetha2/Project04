3fb9d4224416e4ae9f4835a1b878ab2683a82110
Duleetha Madubhashana <148762717+Duleetha2@users.noreply.github.com>
2025-02-20 20:17:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Project04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+            Console.Write("Enter Temprature :");
+            temp = Convert.ToInt32(Console.ReadLine());
+
+            switch(temp)
+            {
+                case 0:
+                    Console.WriteLine("Frezing weather\n");
+                    break;
+                case 10:
+                    Console.WriteLine("very cold weather\n");
+                    break;
+                case 20:
+                    Console.WriteLine("Cold weather\n");
+                    break;
+                case 30:
+                    Console.WriteLine("Normal Temprature\n");
+                    break;
+                case 40:
+                    Console.WriteLine("Hot\n");
+                    break;
+                default:
+                    Console.WriteLine("Very hot\n");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
